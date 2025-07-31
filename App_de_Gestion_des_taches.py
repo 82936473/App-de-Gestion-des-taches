4dffda4c4edbda6b61e10c9919ac1bceb3953407
Ayman <ayman.laa09@gmail.com>
2025-07-31 21:46:06 +0100

    Ajouter le sevegarde avec fichiers

--- App_de_Gestion_des_taches.py
+++ App_de_Gestion_des_taches.py
@@ -1,8 +1,6 @@
+import os
 import time
 t=time
-comptes={}
-codes={}
-corbeille={}
 nom_utili = '' 
 code = '' 
 def clear():
@@ -15,11 +13,13 @@ def codde():
          code = input("Creer un mot de passe : ")
          veerification = input('Verifier le mot de passe : ')
          if veerification==code:
-            codes[nom_utili]=code
-
+            os.makedirs(nom_utili)
+            open(os.path.join(nom_utili,'perso_infos'),'w')
+            open(os.path.join(nom_utili,'taches'),'w')
+            open(os.path.join(nom_utili,'corbeille'),'w')
+            with open(f"{nom_utili}/perso_infos",'r+') as f:
+                f.write(code)
             print('Compte enregistre👍.')  
-            comptes[nom_utili]=''
-            corbeille[nom_utili]=[]
             print("...")
             t.sleep(1.5)
             clear()
@@ -35,7 +35,7 @@ def start():
     if choic_input == "1":
         while True:
             nom_utili = input("Entrer un nom d'utilisateur : ")
-            if nom_utili in comptes:
+            if os.path.exists(nom_utili):
                 print("ce nom d'utilisateur est deja existant. Veuillez saisir un autre.")
                 print('...')
                 t.sleep(1.5)
@@ -45,27 +45,28 @@ def start():
                 codde() 
 
     elif choic_input == "2":
-        if not nom_utili and not code: # verification si la valeur est initiale 
-            print("Aucun compte n'a été créé. Veuillez d'abord créer un compte.")
-            print('...')
-            t.sleep(2.5)
-            clear()
-            start() 
         print('----Connexion----')
         alre_nom_utili = input("Entrer votre nom d'utilisateur : ")
         alre_code = input("Mot de passe : ")
-        if alre_nom_utili in codes:
-            if alre_nom_utili in codes.keys() and alre_code==codes[alre_nom_utili] :
-                print('Entree reussi👍.')
-                print('...')
-                t.sleep(1)
-                clear()
-                general()
-            else:
-                print("Nom d'utilisateur ou mot de passe incorrect.")
-                start()
+        if os.path.isdir(alre_nom_utili) :
+            with open(f"{alre_nom_utili}/perso_infos",'r+') as f:
+                line=f.readline()
+                if alre_code==line:
+                    print('Entree reussi👍.')
+                    print('...')
+                    t.sleep(1)
+                    clear()
+                    general()
+                else:
+                    print("Mot de passe incorrect.")
+                    t.sleep(0.5)
+                    clear()
+                    start()
+
         else:
             print("Nom d'utilisateur ou mot de passe incorrect.")
+            t.sleep(1)
+            clear()
             start()
     else:
         print("Choix invalide.")
@@ -73,7 +74,11 @@ def start():
         t.sleep(1)
         clear()
         start()
+
+
 #! la deuxieme tache du code:👇
+
+
 def menu():
     print("(1) Voir tes taches.")
     print("(2) Ajouter une tache.")
@@ -82,16 +87,19 @@ def menu():
     print("(5) Quitter.")
 def voir():
     clear()
-    if not comptes[alre_nom_utili]:
+    if os.path.getsize(f"{nom_utili}/taches")==0:
         print("Vous n'avez pas de taches.")
         print("...")
         t.sleep(1.5)
+        clear()
         general()
     else:
         print("\nVotre taches :")
-        for index , tache in enumerate(comptes[alre_nom_utili],1):
-            print(f"{index}: {tache}")        
-            t.sleep(0.4)                   
+        with open(f"{nom_utili}/taches",'r+') as f:
+            lines=f.readlines()
+            for ind,i in enumerate(lines,1):
+                print(f"{ind} : {i}")
+                t.sleep(0.4)                
 def ajouter():
         print("Entrer vos taches, cliquer entrer pour passer a la tache suivante entrer 'Fin' pour finir l'ajout de taches.")
         taches=[]
@@ -103,19 +111,20 @@ def ajouter():
                     t.sleep(1)
                     clear()
                     break
-                print("les taches sont ajoutees avec succèes")
-                if not comptes[alre_nom_utili]:
-                    comptes[alre_nom_utili]=taches
-                else:
+                with open(f"{nom_utili}/taches",'r+') as f:
                     for i in taches:
-                        comptes[alre_nom_utili].append(i)
+                        f.write(i+'\n')
+                print("les taches sont ajoutees avec succèes")
+
                 print('...')
                 t.sleep(1)
                 clear()
                 general()
             taches.append(tache)
-def supprimer(): 
+def supprimer():
     voir()
+    with open(f"{nom_utili}/taches",'r+') as f:
+        lines=f.readlines()
     try:
         index = input("choisir l'incide du tache a supprimer, ou entrer 'N' pour anuler : ")
         if index.lower()=='n':
@@ -123,12 +132,15 @@ def supprimer():
             t.sleep(1)
             general()
         else:
-            cour=[]
             index_int=int(index)
             index_int-=1
-            supp=comptes[alre_nom_utili].pop(index_int)
-
-            corbeille[alre_nom_utili].append(supp)
+            supp=lines.pop(index_int)
+            with open(f"{nom_utili}/taches",'w+') as f:
+                for i in lines:
+                    f.write(i)
+            with open(f"{nom_utili}/corbeille",'r+') as f:
+                f.seek(0,2)
+                f.write(supp)
             print(f"la tache {index} est supprimer")
             t.sleep(1.5)
             clear()
@@ -138,15 +150,18 @@ def supprimer():
         supprimer()
 def courbeille():
     clear()
-    if  not corbeille[alre_nom_utili]:
-        print("Aucun element dans la courbeille.")
+    if  os.path.getsize(f"{nom_utili}/corbeille")==0:
+        print(" la courbeille est vide.")
         t.sleep(0.5)
         clear()
         general()
     print("Les taches recement supprimer : ")
-    for indice,i in enumerate(corbeille[alre_nom_utili],1):
-        print(f"{indice} : {i}")
-        t.sleep(0.4)
+    with open(f"{nom_utili}/corbeille",'r+') as f:
+        lines=f.readlines()
+    for indice,i in enumerate(lines,1):
+        if i !=0:
+            print(f"{indice} : {i}")
+            t.sleep(0.4)
     index=input("choisir un indice, entrer 'N' pour annuler l'action : ")
     if index.lower()=='n':
         print("Action annulee.")
@@ -156,11 +171,14 @@ def courbeille():
     try:
         index_int=int(index)
         index_int-=1
-        choix=input("Vous voulez supprimer(1), cette tache ou la restorer(2) : ")
+        choix=input("Vous voulez supprimer la tache (1), cette tache ou la restorer (2) : ")
         if choix=='1':
             choi_def=input("cette tache sera supprimer definitivement, Etes vous sure(O/N) :")
             if choi_def.lower()=='o' or choi_def=='0':
-                corbeille[alre_nom_utili].pop(index_int)
+                lines.pop(index_int)
+                with open(f"{nom_utili}/corbeille",'w+') as f:
+                    for i in lines:
+                        f.write(i)
                 print("👍")
                 t.sleep(0.4)
                 clear()
@@ -173,8 +191,12 @@ def courbeille():
                 print("choix invalide.")
                 courbeille()
         elif choix=='2':
-            comptes[alre_nom_utili].append(corbeille[alre_nom_utili][index_int])
-            corbeille[alre_nom_utili].pop(index_int)
+            with open(f"{nom_utili}/taches",'a+') as f:
+                f.write(lines[index_int])
+            lines.pop(index_int)
+            with open(f"{nom_utili}/corbeille",'w+') as f:
+                for i in lines:
+                    f.write(i)
             print('👍')
             t.sleep(0.4)
             clear()
